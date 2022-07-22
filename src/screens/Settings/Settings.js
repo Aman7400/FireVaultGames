40c3c9446f2889d5aedca8c72f9e119e50ac0241
Aman Shukla <amanks7400@gmail.com>
2022-07-22 15:33:18 +0530

    Update Settings Tab

--- src/screens/Settings/Settings.js
+++ src/screens/Settings/Settings.js
@@ -1,14 +1,25 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import SafeAreaWrapper from '../../components/SafeAreaWrapper'
+import { ubuntu } from '../../constants/fonts'
 
 const Settings = () => {
   return (
-    <View>
-      <Text>Settings</Text>
-    </View>
+    <SafeAreaWrapper style={styles.container}>
+      <Text style={{
+        fontFamily : ubuntu.ExtraBold,
+        fontSize : 24,
+      }}>Settings</Text>
+    </SafeAreaWrapper>
   )
 }
 
 export default Settings
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex:1
+  }
+})
