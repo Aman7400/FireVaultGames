f300b68706097966e28c0f6213e6d701e3831c1d
Aman Shukla <amanks7400@gmail.com>
2022-07-22 15:30:40 +0530

    Update Explore Tab

--- src/screens/Explore/Explore.js
+++ src/screens/Explore/Explore.js
@@ -1,14 +1,25 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import SafeAreaWrapper from '../../components/SafeAreaWrapper'
+import { ubuntu } from '../../constants/fonts'
 
 const Explore = () => {
   return (
-    <View>
-      <Text>Explore</Text>
-    </View>
+    <SafeAreaWrapper style={styles.container}>
+      <Text style={{
+        fontFamily : ubuntu.ExtraBold,
+        fontSize : 24,
+      }}>Explore Section Coming Soon</Text>
+    </SafeAreaWrapper>
   )
 }
 
 export default Explore
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    justifyContent:"center",
+    alignItems: "center",
+    flex:1
+  }
+})
